bc98287e25da69c0ebb120f0d2be45d27e26f2f0
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-09-02 18:54:27 -0300

    realized part 1,2 and 3 of tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+// Vetor de heróis
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Spider-Man' },
+  { id: 2, name: 'Iron-Man' },
+  { id: 3, name: 'Thor' },
+  { id: 4, name: 'Captain America' },
+  { id: 5, name: 'Loki' },
+  { id: 6, name: 'Vision' },
+  { id: 7, name: 'Wanda' },
+  { id: 8, name: 'Black Widow' },
+  { id: 9, name: 'Captain Marvel' },
+  { id: 10, name: 'Wonder Woman' }
+];
