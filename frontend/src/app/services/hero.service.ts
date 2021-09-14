d78db580856a753fe40b32de96e0e965f076bb44
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-09-14 16:30:32 -0300

    adicionando url local do servico

--- frontend/src/app/services/hero.service.ts
+++ frontend/src/app/services/hero.service.ts
@@ -44,7 +44,8 @@ export class HeroService {
 
   // Atualiza os dados de um herói no servidor e retorna um observable do herói.
   updateHero(hero: Hero): Observable<any> {
-    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http.put(url, hero, this.httpOptions).pipe(
       tap(_ => this.log(`updated hero id=${hero.id}`)),
       catchError(this.handleError<any>('updateHero'))
     );
