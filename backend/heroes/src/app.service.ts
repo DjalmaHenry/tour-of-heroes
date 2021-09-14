bd74def55dc1f47b63fe58045894225835e01743
Djalma Henrique Silva Lima <45500812+DjalmaHenry@users.noreply.github.com>
2021-09-14 18:24:07 -0300

    adicionando herois ao servico

--- backend/heroes/src/app.service.ts
+++ backend/heroes/src/app.service.ts
@@ -3,7 +3,18 @@ import { Hero } from './hero';
 
 @Injectable()
 export class AppService {
-  heroes: Hero[] = [];
+  heroes: Hero[] = [
+    { id: 1, name: 'Spider-Man' },
+    { id: 2, name: 'Iron-Man' },
+    { id: 3, name: 'Thor' },
+    { id: 4, name: 'Captain America' },
+    { id: 5, name: 'Loki' },
+    { id: 6, name: 'Vision' },
+    { id: 7, name: 'Wanda' },
+    { id: 8, name: 'Black Widow' },
+    { id: 9, name: 'Captain Marvel' },
+    { id: 10, name: 'Wonder Woman' },
+  ];
 
   // pega todas as mensagens
   getAll() {
